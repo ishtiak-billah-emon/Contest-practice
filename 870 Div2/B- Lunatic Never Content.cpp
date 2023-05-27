699b1500456cd959a54b80115141310dcf8cf4f6
Ishtiak Billah Emon <121837437+ishtiak-billah-emon@users.noreply.github.com>
2023-05-27 18:56:47 +0600

    Code correction

--- 870 Div2/B- Lunatic Never Content.cpp
+++ 870 Div2/B- Lunatic Never Content.cpp
@@ -6,55 +6,15 @@ int main()
     while(t--)
     {
         int n ; cin>>n;
+        int ar[n];
+        for(int i=0;i<n;i++)
+            cin >> ar[i];
 
-        long long arr[n]={0};
+        int ans=0;
+        for(int i=0;i<n;i++)
+            ans=__gcd(ans,abs(ar[i]-ar[n-1-i]));
 
-
-        for(int i = 0 ; i<n;i++)
-        {
-           cin>>arr[i];
-        }
-        if( (count (arr,arr+n,0)) == n)
-        {
-            cout<<0<<"\n";
-            continue;
-        }
-
-       long long  ans = INT_MIN;
-
-
-
-      long long maximum = INT_MIN;
-      long long minimum = INT_MAX;
-
-      for(int i = 0 ;i<n;i++)
-      {
-          maximum = max(arr[i],maximum);
-          minimum = min(arr[i],minimum);
-      }
-       long long i = 0 ,j = maximum;
-
-     while(i < n && j >= 0 )
-     {
-         if( ( arr[i] % j ) == ( arr[n-1-i] %j ) )
-         {
-             i++;
-             if(i > (n-1-i))
-             {
-
-                 ans = j;
-             }
-         }
-         else{
-            i = 0;
-            j--;
-         }
-
-     }
-     cout<<ans<<endl;
-
-
-
+        cout << ans << "\n";
 
     }
 }
